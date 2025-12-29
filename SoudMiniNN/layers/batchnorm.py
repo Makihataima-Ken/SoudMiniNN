6b894408f626d0ad5d9a606d3de3403b25599aa9
Ken <ahmadsoud63@gmail.com>
2025-12-30 00:21:54 +0300

    Minor changes

--- SoudMiniNN/layers/batchnorm.py
+++ SoudMiniNN/layers/batchnorm.py
@@ -3,16 +3,13 @@ from .base_layer import Layer
 
 class BatchNorm(Layer):
     def __init__(self, input_size, momentum=0.9):
-        # FIX: Initialize gamma and beta as learnable parameters of the correct shape
         self.gamma = np.ones((1, input_size))
         self.beta = np.zeros((1, input_size))
         self.momentum = momentum
-        
-        # These will be initialized on the first forward pass
+
         self.running_mean = None
         self.running_var = None
 
-        # Placeholders for backward pass
         self.batch_size = None
         self.xc = None
         self.std = None
@@ -20,11 +17,9 @@ class BatchNorm(Layer):
         self.dgamma = None
         self.dbeta = None
 
-    # FIX: Corrected spelling of the 'training' argument
     def forward(self, x, training=True):
         
         if self.running_mean is None:
-            # Initialize running_mean and running_var on the first pass
             self.running_mean = np.zeros(x.shape[1])
             self.running_var = np.zeros(x.shape[1])
 
@@ -35,17 +30,14 @@ class BatchNorm(Layer):
             std = np.sqrt(var + 10e-7)
             xn = xc / std
 
-            # Store values needed for backward pass
             self.batch_size = x.shape[0]
             self.xc = xc
             self.xn = xn
             self.std = std
 
-            # Update running stats for inference
             self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * mu
             self.running_var = self.momentum * self.running_var + (1 - self.momentum) * var
         else:
-            # Use running stats during inference
             xc = x - self.running_mean
             xn = xc / (np.sqrt(self.running_var + 10e-7))
 
@@ -53,11 +45,9 @@ class BatchNorm(Layer):
         return out
     
     def backward(self, grad):
-        # Calculate gradients for gamma and beta first
         self.dbeta = grad.sum(axis=0, keepdims=True)
         self.dgamma = np.sum(self.xn * grad, axis=0, keepdims=True)
 
-        # Propagate gradient back to the input
         dxn = self.gamma * grad
         dxc = dxn / self.std
         dstd = -np.sum((dxn * self.xc) / (self.std * self.std), axis=0)
--- SoudMiniNN/losses/softmax_func.py
+++ SoudMiniNN/losses/softmax_func.py
@@ -3,8 +3,7 @@ from .base_loss import Loss
 
 class SoftmaxCrossEntropy(Loss):
     def forward(self, logits, y_true):
-        exp = np.exp(logits - np.max(logits, axis=1, keepdims=True))
-        self.probs = exp / np.sum(exp, axis=1, keepdims=True)
+        self.probs = softmax(logits)
         self.y_true = y_true
         log_likelihood = -np.log(self.probs[range(len(y_true)), y_true])
         return np.mean(log_likelihood)
@@ -13,3 +12,16 @@ class SoftmaxCrossEntropy(Loss):
         grad = self.probs.copy()
         grad[range(len(self.y_true)), self.y_true] -= 1
         return grad / len(self.y_true)
+    
+def softmax(x):
+    exp = np.exp(x - np.max(x, axis=-1, keepdims=True))
+    return exp / np.sum(exp, axis=-1, keepdims=True)
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
