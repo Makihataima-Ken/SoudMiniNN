8e1e7ddc8502cd8f480ad0b8feaa9f26e8cafe1e
TariqSp <teto75928@gmail.com>
2026-01-27 14:50:30 +0300

    edit activations layar

--- SoudMiniNN/layers/activations.py
+++ SoudMiniNN/layers/activations.py
@@ -1,8 +1,12 @@
 import numpy as np
-from .base_layer import Layer
+from ..core.module import Module
 
-class ReLU(Layer):
-    def forward(self, x, training=True):
+class ReLU(Module):
+    def __init__(self):
+        super().__init__()
+        self.mask = None
+
+    def forward(self, x):
         self.mask = x > 0
         return x * self.mask
 
@@ -10,19 +14,42 @@ class ReLU(Layer):
         return grad * self.mask
 
 
-class Sigmoid(Layer):
-    def forward(self, x, training=True):
-        self.out = 1 / (1 + np.exp(-x))
+class Sigmoid(Module):
+    def __init__(self):
+        super().__init__()
+        self.out = None
+
+    def forward(self, x):
+        self.out = 1.0 / (1.0 + np.exp(-x))
         return self.out
 
     def backward(self, grad):
-        return grad * self.out * (1 - self.out)
+        return grad * self.out * (1.0 - self.out)
 
 
-class Tanh(Layer):
-    def forward(self, x, training=True):
-        self.out = np.tanh(x)
+class Softmax(Module):
+    """
+    Usually you DON'T need Softmax as a layer when using CrossEntropyLoss on logits.
+    Keep it for inference/visualization.
+    """
+    def __init__(self, axis: int = 1):
+        super().__init__()
+        self.axis = axis
+        self.out = None
+
+    def forward(self, x):
+        x_shift = x - np.max(x, axis=self.axis, keepdims=True)
+        exp_x = np.exp(x_shift)
+        self.out = exp_x / np.sum(exp_x, axis=self.axis, keepdims=True)
         return self.out
 
     def backward(self, grad):
-        return grad * (1 - self.out ** 2)
+        # general softmax backward is expensive; for training use CrossEntropyLoss(logits)
+        # Here we implement a correct but slower version.
+        y = self.out
+        dx = np.empty_like(grad)
+        for i in range(grad.shape[0]):
+            yi = y[i].reshape(-1, 1)
+            J = np.diagflat(yi) - yi @ yi.T
+            dx[i] = (J @ grad[i].reshape(-1, 1)).reshape(-1)
+        return dx
