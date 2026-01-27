59d5bc95399049559d4d20cc59f9847298963fde
TariqSp <teto75928@gmail.com>
2026-01-27 15:16:20 +0300

    edit base optimizer

--- SoudMiniNN/optimizers/base_optimizer.py
+++ SoudMiniNN/optimizers/base_optimizer.py
@@ -1,6 +1,16 @@
 from abc import ABC, abstractmethod
+from typing import Iterable, List
+from ..core.parameter import Parameter
 
 class Optimizer(ABC):
+    def __init__(self, params: Iterable[Parameter], lr: float):
+        self.params: List[Parameter] = list(params)
+        self.lr = lr
+
+    def zero_grad(self) -> None:
+        for p in self.params:
+            p.zero_grad()
+
     @abstractmethod
-    def update(self, params:dict, grads:dict):
+    def step(self) -> None:
         pass
