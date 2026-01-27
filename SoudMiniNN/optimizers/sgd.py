c793655048cc217c2a30791fe4a5474591719a40
TariqSp <teto75928@gmail.com>
2026-01-27 15:17:06 +0300

    sgd optimal

--- SoudMiniNN/optimizers/sgd.py
+++ SoudMiniNN/optimizers/sgd.py
@@ -1,9 +1,15 @@
 from .base_optimizer import Optimizer
 
 class SGD(Optimizer):
-    def __init__(self, lr=0.01):
-        self.lr = lr
+    def __init__(self, params, lr: float = 1e-2, weight_decay: float = 0.0):
+        super().__init__(params, lr)
+        self.weight_decay = weight_decay
 
-    def update(self, params:dict, grads:dict):
-        for key in params.keys():
-            params[key] -= self.lr * grads[key]
+    def step(self) -> None:
+        for p in self.params:
+            if not p.requires_grad:
+                continue
+            grad = p.grad
+            if self.weight_decay != 0.0:
+                grad = grad + self.weight_decay * p.data
+            p.data[...] = p.data - self.lr * grad
