441dff47f9e123a6e92eb9e459421f4c1505f00d
TariqSp <teto75928@gmail.com>
2026-01-27 15:16:47 +0300

    opt momentum class

--- SoudMiniNN/optimizers/momentum.py
+++ SoudMiniNN/optimizers/momentum.py
@@ -2,16 +2,21 @@ import numpy as np
 from .base_optimizer import Optimizer
 
 class Momentum(Optimizer):
-    def __init__ (self, lr=0.01, momentum=0.9):
-        self.lr = lr
+    def __init__(self, params, lr: float = 1e-2, momentum: float = 0.9, weight_decay: float = 0.0):
+        super().__init__(params, lr)
         self.momentum = momentum
-        self.v = None
-    def update(self, params:dict, grads:dict):
-        if self.v is None:
-          self.v = {}
-          for key, val in params.items():
-            self.v[key] = np.zeros_like(val)
-            
-        for key in params.keys():
-          self.v[key] = self.momentum*self.v[key] - self.lr*grads[key]
-          params[key] += self.v[key]
+        self.weight_decay = weight_decay
+        self.v = {}
+
+    def step(self) -> None:
+        for p in self.params:
+            if not p.requires_grad:
+                continue
+            key = id(p)
+            if key not in self.v:
+                self.v[key] = np.zeros_like(p.data)
+            grad = p.grad
+            if self.weight_decay != 0.0:
+                grad = grad + self.weight_decay * p.data
+            self.v[key] = self.momentum * self.v[key] - self.lr * grad
+            p.data[...] = p.data + self.v[key]
