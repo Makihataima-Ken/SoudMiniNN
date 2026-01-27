6996c31440a0774633fcd226e0bb2736db2ce7f4
TariqSp <teto75928@gmail.com>
2026-01-27 15:12:23 +0300

    add flatten layar

---
+++ SoudMiniNN/layers/flatten.py
@@ -0,0 +1,17 @@
+import numpy as np
+from ..core.module import Module
+
+class Flatten(Module):
+    """Flatten (N, C, H, W) or (N, ...) into (N, D)."""
+    def __init__(self):
+        super().__init__()
+        self._orig_shape: tuple[int, ...] | None = None
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        self._orig_shape = x.shape
+        return x.reshape(x.shape[0], -1)
+
+    def backward(self, dout: np.ndarray) -> np.ndarray:
+        if self._orig_shape is None:
+            raise RuntimeError("Flatten.backward called before forward")
+        return dout.reshape(self._orig_shape)
